afaa06a6319991967a064b72fca44a5bda5c5c78
WorldThirteen <mihailzachepilo@gmail.com>
2021-03-22 16:03:55 +0200

    fix(mozilla-tl): Make a temporary fix for issue with mozilla long cert file name

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -102,14 +102,14 @@ function parseEUTLTrusted() {
   let eutl = new tl_create.EUTL();
   let tl = eutl.getTrusted();
 
-  Promise.all(eutl.TrustServiceStatusLists.map(function(list) { return list.CheckSignature(); }))
-    .then(function(verify) {
+  Promise.all(eutl.TrustServiceStatusLists.map(function (list) { return list.CheckSignature(); }))
+    .then(function (verify) {
       if (!verify)
         console.log("Warning!!!: EUTL signature is not valid");
       else
         console.log("Information: EUTL signature is valid");
     })
-    .catch(function(e) {
+    .catch(function (e) {
       console.log("Error:", e.message);
     });
 
@@ -167,13 +167,13 @@ function parseCiscoTrusted(ciscoType: string) {
   let cisco = new tl_create.Cisco(ciscoType);
   let tl = cisco.getTrusted();
   cisco.verifyP7()
-    .then(function(verify) {
+    .then(function (verify) {
       if (!verify)
         console.log("Warning!!!: Cisco PKCS#7 signature verification failed");
       else
         console.log("Information: Cisco PKCS#7 signature verification successful");
     })
-    .catch(function(e) {
+    .catch(function (e) {
       console.log("Error:", e);
     });
   return tl;
@@ -386,7 +386,20 @@ switch ((program.format || "pem").toLowerCase()) {
               n: files[k].nameID
             });
 
-            fs.writeFileSync(targetDir + "/" + files[k].name, Buffer.from(files[k].content));
+            // TODO: temporary workaround issue with mozilla cert
+            try {
+              fs.writeFileSync(targetDir + "/" + files[k].name, Buffer.from(files[k].content));
+
+              filesJSON[directory].push({
+                k: files[k].name,
+                n: files[k].nameID
+              });
+            } catch (err) {
+              if (err.code !== "ENAMETOOLONG") {
+                throw err;
+              }
+              console.log(err.message);
+            }
           }
         }
 
