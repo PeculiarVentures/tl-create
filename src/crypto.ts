b96700c1d941c99188b6c5a352e4a5624370caab
Mykhailo Zachepylo <mihailzachepilo@gmail.com>
2026-03-17 13:53:12 +0200

    Fix XML RSA PSS verification

--- src/crypto.ts
+++ src/crypto.ts
@@ -1,5 +1,6 @@
 import * as asn1js from "asn1js";
 import * as pkijs from "pkijs";
+import * as XmlDSigJs from "xmldsigjs";
 import { Crypto } from "@peculiar/webcrypto";
 
 export { asn1js, pkijs };
@@ -11,3 +12,8 @@ pkijs.setEngine(cryptoName, crypto, new pkijs.CryptoEngine({
   crypto: crypto,
   subtle: crypto.subtle
 }));
+
+XmlDSigJs.CryptoConfig.RegisterSignatureAlgorithm(XmlDSigJs.RSA_PSS_SHA1_NAMESPACE, XmlDSigJs.RsaPssWithoutParamsSha1 as any);
+XmlDSigJs.CryptoConfig.RegisterSignatureAlgorithm(XmlDSigJs.RSA_PSS_SHA256_NAMESPACE, XmlDSigJs.RsaPssWithoutParamsSha256 as any);
+XmlDSigJs.CryptoConfig.RegisterSignatureAlgorithm(XmlDSigJs.RSA_PSS_SHA384_NAMESPACE, XmlDSigJs.RsaPssWithoutParamsSha384 as any);
+XmlDSigJs.CryptoConfig.RegisterSignatureAlgorithm(XmlDSigJs.RSA_PSS_SHA512_NAMESPACE, XmlDSigJs.RsaPssWithoutParamsSha512 as any);
